c5a21b4ce5b15ef357cd8ccead3edcb93918709e
OPOII <bryancamili198@hotmail.com>
2023-08-15 15:37:25 -0500

    selector de regiones

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Region } from '../interfaces/country.interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountriesService {
 
+  private _regions:Region[]=[Region.Africa,Region.Americas,Region.Asia,Region.Europe,Region.Oceania,]
+
   constructor() { }
+
+  get regions():Region[]{
+    // Con esto rompo la relación con las regiones y evito que lo dañen
+    return [...this._regions];
+  }
+
 }
