d7934138a86207debadc134899f712b5de2dec22
OPOII <bryancamili198@hotmail.com>
2023-08-16 00:02:13 -0500

    Tercer selector anidado-fronteras

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
 import { Region, SmallCountry } from '../../interfaces/country.interfaces';
-import { switchMap, tap } from 'rxjs';
+import { filter, switchMap, tap } from 'rxjs';
 @Component({
   selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
@@ -13,10 +13,11 @@ export class SelectorPageComponent implements OnInit{
 
 
   public countriesByRegion: SmallCountry[]=[];
+  public borders: string[]=[];
   public myForm: FormGroup=this.fb.group({
     region:['',Validators.required],
     country:['',Validators.required],
-    borders:['',Validators.required],
+    border:['',Validators.required],
   })
 
   constructor(
@@ -26,6 +27,7 @@ export class SelectorPageComponent implements OnInit{
   // Se ejecuta cuando se esta inicializando
   ngOnInit(): void {
    this.onRegionChanged();
+   this.onCountryChange();
   }
 
   get regions():Region[]{
@@ -36,6 +38,7 @@ export class SelectorPageComponent implements OnInit{
     this.myForm.get('region')!.valueChanges
     .pipe(
       tap(()=>this.myForm.get('country')!.setValue('')),
+      tap(()=>this.borders=[]),
       // Me permite recibir un observable y suscribirme a otro observable
       switchMap((region)=>this.countriesService.getcountriesByRegion(region))
     )
@@ -44,5 +47,18 @@ export class SelectorPageComponent implements OnInit{
     });
   }
 
+  onCountryChange():void{
+    this.myForm.get('country')!.valueChanges
+    .pipe(
+      tap(()=>this.myForm.get('border')!.setValue('')),
+      filter((value:string)=>value.length>0),
+      // Me permite recibir un observable y suscribirme a otro observable
+      switchMap((alphacode)=>this.countriesService.getCountryByAlphaCode(alphacode))
+    )
+    .subscribe(country=>{
+      this.borders=country.borders;
+      // this.countriesByRegion=countries
+    });
+  }
 
 }
